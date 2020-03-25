46fc5e44d5c224d525cf4fc70a8535bbe33c9175
lesliedainelli <leslie.dainelli@live.fr>
2020-03-25 15:47:33 +0100

    test

---
+++ Code_E1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Enigme 1</title>
+    </head>
+
+    <body>
+    	<header>
+    	 	<h1 class="titre">CODE</h1>
+		</header>
+		
+
+
+		<form action="cible.php" method="post">
+				<p>
+					<input type="text" name="prenom" />
+					<input type="submit" value="Valider" />
+				</p>
+		</form>
+
+		<p class="boutonValider"><a href="Serious Game 1.html" title="Cliquez pour lancer la partie et le compte à rebours."><img src="valider_btn.png"></a></p>
+
+
+    	<!--<footer>
+    		<p class="contact"><a href="mailto:votrenom@bidule.com">Contacter DAP.</a></p>    	
+    	</footer>-->
+    </body>
+</html>
